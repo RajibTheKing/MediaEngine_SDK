66973295e701732dbdaa769955879ffd9e738491
Samaun <anayetul@ipvisionsoft.com>
2016-02-06 19:30:31 +0600

    Added Synchronized Map class to the project

---
+++ VideoEngineController/SynchronizedMap.cpp
@@ -0,0 +1,54 @@
+
+#include "SynchronizedMap.h"
+
+#include <string.h>
+#include "LogPrinter.h"
+
+CSynchronizedMap::CSynchronizedMap()
+{
+	m_pSynchronizedMapMutex.reset(new CLockHandler);
+}
+
+CSynchronizedMap::~CSynchronizedMap()
+{
+/*	if (m_pSynchronizedMapMutex.get())
+		m_pSynchronizedMapMutex.reset();*/
+}
+
+void CSynchronizedMap::clear()
+{
+	Locker lock(*m_pSynchronizedMapMutex);
+
+	m_STLMap.clear();
+}
+
+int CSynchronizedMap::find(int index)
+{
+	Locker lock(*m_pSynchronizedMapMutex);
+
+	if(m_STLMap.find(index) == m_STLMap.end())
+	{
+		return -1;
+	}
+	else
+		return m_STLMap[index];
+}
+
+int CSynchronizedMap::getElementAt(int index)
+{
+	Locker lock(*m_pSynchronizedMapMutex);
+
+	return m_STLMap[index];
+}
+
+int CSynchronizedMap::end()
+{
+	return -1;
+}
+
+int CSynchronizedMap::insert(int index, int element)
+{
+	Locker lock(*m_pSynchronizedMapMutex);
+
+	m_STLMap[index] = element;
+}
