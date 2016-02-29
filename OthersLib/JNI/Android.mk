2ced2792c6cedad6782b24fdb4cc4175fee9f768
Samaun <anayetul@ipvisionsoft.com>
2016-02-29 20:41:22 +0600

    VideoDecodingClass Added for android

--- OthersLib/JNI/Android.mk
+++ OthersLib/JNI/Android.mk
@@ -69,6 +69,7 @@ LOCAL_SRC_FILES := \
             ../../../videoengine/VideoEngineController/VideoDecoder.cpp \
 			../../../videoengine/VideoEngineController/VideoEncoder.cpp \
 			../../../videoengine/VideoEngineController/VideoEncodingThread.cpp \
+			../../../videoengine/VideoEngineController/VideoDecodingThread.cpp \
 			../../../videoengine/VideoEngineController/RenderingThread.cpp \
 			../../../videoengine/VideoEngineController/SendingBuffer.cpp \
 			../../../videoengine/VideoEngineController/RenderingBuffer.cpp \
