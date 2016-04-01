3b9a85f56d3009bfb6ca17e38e71c11b0a17f4c2
Samaun <anayetul@ipvisionsoft.com>
2016-04-01 20:26:33 +0600

    Removed unncessary codes and variables from RetransmitVideoPacketQueue

--- VideoEngineController/RetransmitVideoPacketQueue.h
+++ VideoEngineController/RetransmitVideoPacketQueue.h
@@ -11,8 +11,6 @@
 
 using namespace std;
 
-
-
 class CRetransmitVideoPacketQueue
 {
 
@@ -25,32 +23,18 @@ public:
 	int DeQueue(unsigned char *decodeBuffer);
 	void IncreamentIndex(int &index);
 	int GetQueueSize();
-	bool PacketExists(int iFrameNUmber, int iPacketNumber);
 
 private:
 
 	int m_iPushIndex;
 	int m_iPopIndex;
-	int m_iDecodingIndex;
 	int m_iQueueCapacity;
 	int m_iQueueSize;
 
 	unsigned char m_Buffer[MAX_RETRANS_VIDEO_PACKET_QUEUE_SIZE][MAX_VIDEO_PACKET_SIZE];
 	int m_BufferDataLength[MAX_RETRANS_VIDEO_PACKET_QUEUE_SIZE];
-	int m_BufferIndexState[MAX_RETRANS_VIDEO_PACKET_QUEUE_SIZE];
 
 	SmartPointer<CLockHandler> m_pChannelMutex;
-
-
-	/*pair<int, int> ExpectedFramePacketPair;
-	int iNumberOfPacketsInCurrentFrame;
-	pair<int, int> GetFramePacketFromHeader(unsigned char * packet, int &iNumberOfPackets);
-	void UpdateExpectedFramePacketPair(pair<int, int> currentFramePacketPair, int iNumberOfPackets);
-
-	int GetIntFromChar(unsigned char *packetData, int index);
-
-	std::queue<pair<int,int>> ExpectedFramePacketDeQueue;*/
-
 };
 
-#endif //_VIDEO_PACKET_QUEUE_H_
+#endif
