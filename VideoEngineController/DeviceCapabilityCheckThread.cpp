94449f2922dcfe7b5517503daf248f9ef2e8d08b
RajibTheKing <aporba.das@gmail.com>
2016-05-09 12:21:12 +0600

    removed unnecessary code

--- VideoEngineController/DeviceCapabilityCheckThread.cpp
+++ VideoEngineController/DeviceCapabilityCheckThread.cpp
@@ -135,74 +135,7 @@ void CDeviceCapabilityCheckThread::DeviceCapabilityCheckThreadProcedure()
                 printf("Samaun--> STOP_DEVICE_CHECK, iVideoWidth,iVideoHeight = %d,%d ....... Notification = %d\n", nVideoWidth, nVideoHeigth, nNotification);
 				m_pCController->StopTestAudioCall(llFriendID);
 				m_pCController->StopTestVideoCall(llFriendID);
-/*
-#ifdef __ANDROID__
-				if(nNotification == DEVICE_CHECK_SUCCESS && nVideoHeigth == 480)
-				{
-					m_pCController->m_nSupportedResolutionFPSLevel = SUPPORTED_RESOLUTION_FPS_640_25;
 
-					m_pCommonElementBucket->m_pEventNotifier->fireVideoNotificationEvent(llFriendID, m_pCommonElementBucket->m_pEventNotifier->SET_CAMERA_RESOLUTION_640x480_25FPS);
-				}
-				else if(nNotification == DEVICE_CHECK_FAILED && nVideoHeigth == 480)
-				{
-					m_pCommonElementBucket->m_pEventNotifier->fireVideoNotificationEvent(llFriendID, m_pCommonElementBucket->m_pEventNotifier->SET_CAMERA_RESOLUTION_640x480_25FPS_NOT_SUPPORTED);
-				}
-				else if(nNotification == DEVICE_CHECK_SUCCESS && nVideoHeigth<480)
-				{
-					m_pCController->m_nSupportedResolutionFPSLevel = SUPPORTED_RESOLUTION_FPS_352_25;
-
-					m_pCommonElementBucket->m_pEventNotifier->fireVideoNotificationEvent(llFriendID, m_pCommonElementBucket->m_pEventNotifier->SET_CAMERA_RESOLUTION_352x288_25FPS);
-				}
-				else if(nNotification == DEVICE_CHECK_FAILED && nVideoHeigth<480)
-				{
-					m_pCController->m_nSupportedResolutionFPSLevel = SUPPORTED_RESOLUTION_FPS_352_15;
-
-					m_pCommonElementBucket->m_pEventNotifier->fireVideoNotificationEvent(llFriendID, m_pCommonElementBucket->m_pEventNotifier->SET_CAMERA_RESOLUTION_352x288_25FPS_NOT_SUPPORTED);
-				}
-
-
-
-				if((nVideoWidth < 480) || (nVideoWidth == 480 && nNotification == DEVICE_CHECK_SUCCESS))
-				{
-					bDeviceCapabilityCheckThreadRunning = false;
-
-				}
-
-#else
-                
-                if(nNotification == DEVICE_CHECK_SUCCESS && nVideoHeigth == 640)
-                {
-					m_pCController->m_nSupportedResolutionFPSLevel = SUPPORTED_RESOLUTION_FPS_640_25;
-
-                    m_pCommonElementBucket->m_pEventNotifier->fireVideoNotificationEvent(llFriendID, m_pCommonElementBucket->m_pEventNotifier->SET_CAMERA_RESOLUTION_640x480_25FPS);
-                }
-                else if(nNotification == DEVICE_CHECK_FAILED && nVideoHeigth == 640)
-                {
-                    m_pCommonElementBucket->m_pEventNotifier->fireVideoNotificationEvent(llFriendID, m_pCommonElementBucket->m_pEventNotifier->SET_CAMERA_RESOLUTION_640x480_25FPS_NOT_SUPPORTED);
-                }
-                else if(nNotification == DEVICE_CHECK_SUCCESS && nVideoHeigth<640)
-                {
-					m_pCController->m_nSupportedResolutionFPSLevel = SUPPORTED_RESOLUTION_FPS_352_25;
-
-                    m_pCommonElementBucket->m_pEventNotifier->fireVideoNotificationEvent(llFriendID, m_pCommonElementBucket->m_pEventNotifier->SET_CAMERA_RESOLUTION_352x288_25FPS);
-                }
-                else if(nNotification == DEVICE_CHECK_FAILED && nVideoHeigth<640)
-                {
-					m_pCController->m_nSupportedResolutionFPSLevel = SUPPORTED_RESOLUTION_FPS_352_15;
-
-                    m_pCommonElementBucket->m_pEventNotifier->fireVideoNotificationEvent(llFriendID, m_pCommonElementBucket->m_pEventNotifier->SET_CAMERA_RESOLUTION_352x288_25FPS_NOT_SUPPORTED);
-                }
-                
-                
-                
-                if((nVideoHeigth < 640) || (nVideoHeigth == 640 && nNotification == DEVICE_CHECK_SUCCESS))
-                {
-                    bDeviceCapabilityCheckThreadRunning = false;
-                    
-                }
-                
-#endif
-*/
                 if(nNotification == DEVICE_CHECK_SUCCESS && nVideoHeigth == 640)
                 {
                     m_pCController->m_nSupportedResolutionFPSLevel = SUPPORTED_RESOLUTION_FPS_640_25;
