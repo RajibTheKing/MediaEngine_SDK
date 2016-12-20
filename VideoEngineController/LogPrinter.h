bcb1d23175045e7bc8e31396bf1afdd3e217b4de
Md. Mansur <mansur@ipvisionsoft.com>
2016-12-20 22:00:21 +0600

    Used header length

--- VideoEngineController/LogPrinter.h
+++ VideoEngineController/LogPrinter.h
@@ -13,6 +13,7 @@
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 #define LOGEF(...) //__android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 #define __LOG(...) //__android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#define PRT(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 
 #else
 
