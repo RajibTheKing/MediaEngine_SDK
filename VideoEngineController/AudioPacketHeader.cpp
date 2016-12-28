9f6e475ccdd397ef71bf2dc948c10307869398b5
Nayeem_Hossain <vampireegg@gmail.com>
2016-12-28 12:32:50 +0600

    AudioPacketHeader added.

---
+++ VideoEngineController/AudioPacketHeader.cpp
@@ -0,0 +1,205 @@
+
+#include "AudioPacketHeader.h"
+#include "LogPrinter.h"
+
+CAudioPacketHeader::CAudioPacketHeader()
+{
+	nNumberOfHeaderElements = sizeof(HeaderBitmap)/sizeof(int);
+
+	int headerSizeInBit = 0;
+	for (int i = 0; i < nNumberOfHeaderElements; i++)
+	{
+		headerSizeInBit += HeaderBitmap[i];
+	}
+
+	m_nHeaderSizeInBit = headerSizeInBit;
+	m_nHeaderSizeInByte = (headerSizeInBit + 7) / 8;
+
+	memset(m_arrllInformation, 0, sizeof(m_arrllInformation));
+	memset(ma_uchHeader, 0, m_nHeaderSizeInByte);
+}
+
+//CAudioPacketHeader::CAudioPacketHeader(unsigned int * Information)
+//{
+//	CAudioPacketHeader();
+//	CopyInformationToHeader(Information);
+//}
+
+CAudioPacketHeader::CAudioPacketHeader(unsigned char *Header)
+{
+	CAudioPacketHeader();
+	CopyHeaderToInformation(Header);
+}
+
+CAudioPacketHeader::~CAudioPacketHeader()
+{
+	memset(m_arrllInformation, 0, sizeof(m_arrllInformation));
+	memset(ma_uchHeader, 0, m_nHeaderSizeInByte);
+}
+
+//int CAudioPacketHeader::CopyInformationToHeader(unsigned int * Information)
+//{
+//	memcpy(m_arrllInformation, Information, m_nHeaderSizeInByte);
+//	for (int i = 0; i < nNumberOfHeaderElements; i++)
+//	{
+//		SetInformation(Information[i], i);
+//	}
+//	return m_nHeaderSizeInByte;
+//}
+
+long long CAudioPacketHeader::GetFieldCapacity(int InfoType)
+{
+	return 1LL << HeaderBitmap[InfoType];
+}
+
+
+int CAudioPacketHeader::GetHeaderInByteArray(unsigned char* data)
+{
+	memcpy(data, ma_uchHeader, m_nHeaderSizeInByte);
+	return m_nHeaderSizeInByte;
+}
+
+int CAudioPacketHeader::GetHeaderSize()
+{
+	return m_nHeaderSizeInByte;
+}
+
+bool CAudioPacketHeader::IsPacketTypeSupported(unsigned int PacketType)
+{
+	int nPacketTypes = sizeof(SupportedPacketTypes) / sizeof(int);
+	for (int i = 0; i < nPacketTypes; i++)
+	{
+		if (SupportedPacketTypes[i] == PacketType) return true;
+	}
+	return false;
+}
+
+bool CAudioPacketHeader::IsPacketTypeSupported()
+{
+	unsigned int iPackeType = GetInformation(PACKETTYPE);
+	return IsPacketTypeSupported(iPackeType);
+}
+
+void CAudioPacketHeader::SetInformation(long long Information, int InfoType)
+{
+    Information = (Information & ((1LL << HeaderBitmap[InfoType]) - 1));
+	m_arrllInformation[InfoType] = Information;
+
+	int infoStartBit = 0;
+	for (int i = 0; i < InfoType; i++)
+	{
+		infoStartBit += HeaderBitmap[i];
+	}
+	int infoStartByte = infoStartBit / 8;
+	int infoStartBitOfByte = infoStartBit % 8;
+
+	int numberOfBitsIn1stByte;
+	if (8 - infoStartBitOfByte >= HeaderBitmap[InfoType])
+	{
+		numberOfBitsIn1stByte = min(HeaderBitmap[InfoType], 8 - infoStartBitOfByte);
+		ma_uchHeader[infoStartByte] &= ~( ((1 << numberOfBitsIn1stByte) - 1) << (8 - infoStartBitOfByte - numberOfBitsIn1stByte) ) ;
+		ma_uchHeader[infoStartByte] |= (Information >> (HeaderBitmap[InfoType] - numberOfBitsIn1stByte)) << (8 - infoStartBitOfByte - numberOfBitsIn1stByte);
+	}
+	else
+	{
+		numberOfBitsIn1stByte = 8 - infoStartBitOfByte;
+		ma_uchHeader[infoStartByte] &=  ~((1<<numberOfBitsIn1stByte) - 1);
+		ma_uchHeader[infoStartByte] |= (Information >> (HeaderBitmap[InfoType] - numberOfBitsIn1stByte));
+
+		int remainingBits = HeaderBitmap[InfoType] - numberOfBitsIn1stByte;
+		int remainingBytes = remainingBits / 8;
+		int nBitsInLastByte = remainingBits % 8;
+		int byte = 1;
+
+		for(int i = 0; i < remainingBytes; i ++)
+		{
+			ma_uchHeader[infoStartByte + byte] = Information >> (HeaderBitmap[InfoType] - numberOfBitsIn1stByte - 8 * byte);
+			byte++;
+		}
+
+		if(nBitsInLastByte)
+		{
+			ma_uchHeader[infoStartByte + byte] &=  ~ ( ( (1<<nBitsInLastByte) - 1) << (8 - nBitsInLastByte));
+			ma_uchHeader[infoStartByte + byte] |= 0xFF & (Information << (8-nBitsInLastByte) );
+		}
+	}
+}
+
+void CAudioPacketHeader::CopyHeaderToInformation(unsigned char *Header)
+{
+	memcpy(ma_uchHeader, Header, m_nHeaderSizeInByte);
+
+	for (int i = 0; i < nNumberOfHeaderElements; i++)
+	{
+		//CLogPrinter_WriteLog(CLogPrinter::INFO, INSTENT_TEST_LOG,"#PutInformationToArray#");
+		PutInformationToArray(i);
+	}
+}
+
+long long CAudioPacketHeader::GetInformation(int InfoType)
+{
+	return m_arrllInformation[InfoType];
+}
+
+void CAudioPacketHeader::PutInformationToArray(int InfoType)
+{
+	unsigned long long Information = 0;
+	int infoStartBit = 0;
+	for (int i = 0; i < InfoType; i++)
+	{
+		infoStartBit += HeaderBitmap[i];
+	}
+	int infoStartByte = infoStartBit / 8;
+	int infoStartBitOfByte = infoStartBit % 8;
+
+    
+	if (infoStartBitOfByte + HeaderBitmap[InfoType] <= 8)//fits in 1 byte
+	{
+		unsigned char temp = (ma_uchHeader[infoStartByte] << infoStartBitOfByte);
+		Information = (temp >>( 8 - HeaderBitmap[InfoType]));
+	}
+	else
+	{
+		int nBitesToCopy = HeaderBitmap[InfoType] + infoStartBitOfByte;
+		int nBytesToCopy = nBitesToCopy / 8 + (nBitesToCopy % 8 != 0);
+		for (int i = 0; i < nBytesToCopy; i++)
+		{
+			Information <<= 8;
+			Information += ma_uchHeader[infoStartByte + i];
+		}
+
+		int shift = ((HeaderBitmap[InfoType] + infoStartBitOfByte ) % 8);
+		if(shift)
+			Information >>= 8 - shift;
+		Information &= (1LL << HeaderBitmap[InfoType]) - 1;
+	}
+	m_arrllInformation[InfoType] = Information;
+}
+
+void CAudioPacketHeader::showDetails(string prefix)
+{
+	PRT("%s #-> "
+		"PT = %lld "
+		"HL = %lld "
+		"NT = %lld "
+		"VC = %lld "
+		"PN = %lld "
+		"PL = %lld "
+		"RECVDSN = %lld "
+		"NPRECVD = %lld "
+		"C = %lld "
+		"SN = %lld "
+		"TS = %lld",
+		prefix.c_str(),
+		m_arrllInformation[0],
+		m_arrllInformation[1],
+		m_arrllInformation[2],
+		m_arrllInformation[3],
+		m_arrllInformation[4],
+		m_arrllInformation[5],
+		m_arrllInformation[6],
+		m_arrllInformation[7],
+		m_arrllInformation[8],
+		m_arrllInformation[9],
+		m_arrllInformation[10]);
+}
---
+++ VideoEngineController/AudioPacketHeader.h
@@ -0,0 +1,89 @@
+#include "Tools.h"
+#include "Size.h"
+
+#define PACKETTYPE 0
+#define HEADERLENGTH 1
+#define NETWORKTYPE 2
+#define VERSIONCODE 3
+#define PACKETNUMBER 4
+#define PACKETLENGTH 5
+#define RECVDSLOTNUMBER 6
+#define NUMPACKETRECVD 7
+#define CHANNELS 8
+#define SLOTNUMBER 9
+#define TIMESTAMP 10
+
+/////////PacketTypes//////
+#define AUDIO_SKIP_PACKET_TYPE 0
+#define AUDIO_NORMAL_PACKET_TYPE 1
+#define AUDIO_NOVIDEO_PACKET_TYPE 2
+#define AUDIO_VERSION_PACKET_TYPE 3
+#define AUDIO_OPUS_PACKET_TYPE 9
+#define AUDIO_NONMUXED_PACKET_TYPE 10
+#define AUDIO_MUXED_PACKET_TYPE 11
+
+#define MAXFIELDSINHEADER 15
+#define MAXHEADERSIZE 100
+
+
+//#define __AUDIO_HEADER_LENGTH__ 7
+
+
+static int HeaderBitmap[] =
+{
+	8 /*PACKETTYPE*/,
+	6 /*HEADERLENGTH*/,
+	2 /*NETWORKTYPE*/,
+	5 /*VERSIONCODE*/,
+	31 /*PACKETNUMBER*/,
+	12 /*PACKETLENGTH*/,
+	3 /*RECVDSLOTNUMBER*/,
+	8 /*NUMPACKETRECVD*/,
+	2 /*CHANNELS*/,
+	3 /*SLOTNUMBER*/,
+	40  /*TIMESTAMP*/
+};
+
+static int SupportedPacketTypes[] =
+{
+	AUDIO_SKIP_PACKET_TYPE,
+	AUDIO_NORMAL_PACKET_TYPE,
+	AUDIO_NOVIDEO_PACKET_TYPE,
+	AUDIO_VERSION_PACKET_TYPE
+}; //Only for Call
+
+
+class CAudioPacketHeader {
+
+
+	unsigned int m_nHeaderSizeInBit;
+	unsigned int m_nHeaderSizeInByte;
+	long long m_arrllInformation[MAXFIELDSINHEADER];
+
+	unsigned char ma_uchHeader[MAXHEADERSIZE];
+	int nNumberOfHeaderElements;
+	//int CopyInformationToHeader(unsigned int * Information);
+	void PutInformationToArray(int InfoType);
+
+public:
+	CAudioPacketHeader();
+	//CAudioPacketHeader(unsigned int * Information);
+	CAudioPacketHeader(unsigned char *Header);
+	~CAudioPacketHeader();
+
+	void CopyHeaderToInformation(unsigned char *Header);
+	int GetHeaderInByteArray(unsigned char* data);
+
+	int GetHeaderSize();
+
+	void SetInformation(long long Information, int InfoType);
+	long long GetInformation(int InfoType);
+
+	long long GetFieldCapacity(int InfoType);
+
+	bool IsPacketTypeSupported(unsigned int PacketType);
+	bool IsPacketTypeSupported();
+
+	void showDetails(string prefix);
+
+};
