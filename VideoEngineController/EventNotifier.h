a14da7a3f8686084ad7916297edee20682555a11
Samaun <anayetul@ipvisionsoft.com>
2017-03-18 15:07:14 +0600

    Video Session failed event added

--- VideoEngineController/EventNotifier.h
+++ VideoEngineController/EventNotifier.h
@@ -47,6 +47,8 @@ public:
 	static const int SET_CAMERA_RESOLUTION_640x480 = 211;
     static const int SET_CAMERA_RESOLUTION_FAILED= 221;
 
+	static const int VIDEO_SESSION_START_FAILED = 121;
+
 	static const int LIVE_CALL_INSET_ON = 231;
 	static const int LIVE_CALL_INSET_OFF = 232;
 
