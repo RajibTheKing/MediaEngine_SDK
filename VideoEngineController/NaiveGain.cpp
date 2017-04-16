519f08d56738381045361475a6550afe9643edbe
Salah Uddin <sm.salahuddin@ipvisionsoft.com>
2017-04-16 16:38:23 +0600

    Add refactored naive gain

---
+++ VideoEngineController/NaiveGain.cpp
@@ -0,0 +1,75 @@
+#include "NaiveGain.h"
+#include "AudioCallSession.h"
+
+
+NaiveGain::NaiveGain() : m_bGainEnabled(false)
+{
+	m_iVolume = DEF_GAIN;
+}
+
+
+NaiveGain::~NaiveGain()
+{
+
+}
+
+int NaiveGain::SetGain(int iGain)
+{
+	if (iGain < 0)
+	{
+		m_bGainEnabled = false;
+		return false;
+	}
+	else if (iGain >= 0)
+	{
+		m_bGainEnabled = true;
+	}
+
+	if (iGain >= 0 && iGain <= MAX_GAIN)
+	{
+		m_iVolume = iGain;
+		ALOG("SetVolume called with: " + Tools::IntegertoStringConvert(iVolume));
+	}
+	else
+	{
+		m_iVolume = DEF_GAIN;
+	}
+
+	return true;
+}
+
+
+int NaiveGain::AddFarEnd(short *sInBuf, int nBufferSize)
+{
+	if (!m_bGainEnabled)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+
+int NaiveGain::AddGain(short *sInBuf, int nBufferSize, bool isLiveStreamRunning)
+{
+	if (!m_bGainEnabled)
+	{
+		return false;
+	}
+
+	for (int i = 0; i < CURRENT_AUDIO_FRAME_SAMPLE_SIZE(isLiveStreamRunning); i++)
+	{
+		int temp = (int)sInBuf[i] * m_iVolume;
+		if (temp > SHRT_MAX)
+		{
+			temp = SHRT_MAX;
+		}
+		if (temp < SHRT_MIN)
+		{
+			temp = SHRT_MIN;
+		}
+		sInBuf[i] = temp;
+	}
+
+	return true;
+}
