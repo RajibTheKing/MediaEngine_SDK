0868d6ccf6355f16c4876a9dfb486812b1c98709
Salah Uddin <sm.salahuddin@ipvisionsoft.com>
2017-04-16 16:39:28 +0600

    Add gain interface

---
+++ VideoEngineController/AudioGainInterface.h
@@ -0,0 +1,26 @@
+#ifndef AUDIO_GAIN_INTERFACE_H
+#define AUDIO_GAIN_INTERFACE_H
+
+
+#define MAX_GAIN 10
+#define DEF_GAIN 3
+
+class AudioGainInterface
+{
+
+public:
+
+	virtual int SetGain(int iGain) = 0;
+
+	virtual int AddFarEnd(short *sInBuf, int nBufferSize) = 0;
+
+	virtual int AddGain(short *sInBuf, int nBufferSize, bool isLiveStreamRunning) = 0;
+
+	virtual ~AudioGainInterface() { }
+};
+
+
+#endif  // !AUDIO_GAIN_INTERFACE_H
+
+
+// AudioGainInterface <-- WebRTCGain / CGomGomGain / NaiveGain <-- AudioGainInstanceProvider 
