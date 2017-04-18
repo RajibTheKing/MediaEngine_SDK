61ba0f748bea67b5f545b822c801c242778d9383
Salah Uddin <sm.salahuddin@ipvisionsoft.com>
2017-04-18 12:47:26 +0600

    Remove struct format.

--- VideoEngineController/AudioEncoderInterface.h
+++ VideoEngineController/AudioEncoderInterface.h
@@ -2,23 +2,10 @@
 #define AUDIO_ENCODER_INTERFACE_H
 
 
-struct EncoderConfig
-{
-	int bitrate;
-	int complexity;
-
-	EncoderConfig() {
-		bitrate = -1;
-		complexity = -1;
-	}
-};
-
 
 class AudioEncoderInterface
 {
 public:
-
-	virtual bool SetEncoderConfig(EncoderConfig encoderConfig) = 0;
 
 	virtual int EncodeAudio(short *in_data, unsigned int in_size, unsigned char *out_buffer) = 0;
 
