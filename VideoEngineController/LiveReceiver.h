830f21da0cc43555ccfc049de0e041974bd92f6d
Samaun <anayetul@ipvisionsoft.com>
2017-04-22 20:04:57 +0600

    LiveReceiver modified

--- VideoEngineController/LiveReceiver.h
+++ VideoEngineController/LiveReceiver.h
@@ -9,25 +9,26 @@
 #include<vector>
 class CCommonElementsBucket;
 
-class LiveReceiver {
+class LiveReceiver 
+{
+
 public:
+
 	LiveReceiver(CCommonElementsBucket* sharedObject);
     ~LiveReceiver();
 
     void SetVideoDecodingQueue(LiveVideoDecodingQueue *pQueue);    
 	void PushVideoData(unsigned char* uchVideoData, int iLen, int numberOfFrames = 0, int *frameSizes = NULL, int numberOfMissingFrames = 0, int *missingFrames = NULL);
 	void PushVideoDataVector(int offset, unsigned char* uchVideoData, int iLen, int numberOfFrames, int *frameSizes, std::vector< std::pair<int, int> > vMissingFrames);
-    bool GetVideoFrame(unsigned char* uchVideoFrame,int iLen);	
 	
 private:
 
 	Tools m_Tools;
-	
     SmartPointer<CLockHandler> m_pLiveReceiverMutex;
+
     LiveVideoDecodingQueue *m_pLiveVideoDecodingQueue;    
 	CCommonElementsBucket* m_pCommonElementsBucket;
-	// FILE* logFile;
 };
 
 
-#endif //LIVESTREAMING_LIVERECEIVER_H
+#endif 
