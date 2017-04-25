942c5d5570a82794831f6a8523a60ba8fb447b78
Samaun <anayetul@ipvisionsoft.com>
2017-04-25 16:31:31 +0600

    Stop sending IDR request while idr option is false

--- VideoEngineController/VideoCallSession.cpp
+++ VideoEngineController/VideoCallSession.cpp
@@ -802,6 +802,9 @@ void CVideoCallSession::CreateAndSendMiniPacket(int nByteReceivedOrNetworkType, 
 
 void CVideoCallSession::CreateAndSend_IDR_Frame_Info_Packet(long long llMissedFrameNumber)
 {
+	if (m_bDynamic_IDR_Sending_Mechanism == false)
+		return;
+
     CVideoHeader PacketHeader;
     unsigned char uchVersion = (unsigned char)GetVersionController()->GetCurrentCallVersion();
     
