11ab885fb3534d92fd5f684877b7ebc37f5c2ef8
Salah Uddin <sm.salahuddin@ipvisionsoft.com>
2017-04-25 18:24:41 +0600

    Add file for Audio Session Options decider.

--- VideoEngineController/Controller.h
+++ VideoEngineController/Controller.h
@@ -26,25 +26,6 @@ typedef struct
     
 } VideoQuality;
 
-typedef struct
-{
-	int encoderType;
-	int decoderType;
-	
-	bool enableANR;
-	bool enableECM;
-	bool enableAGC;
-	bool bufferData;
-	bool enableMuxing;
-
-	bool adaptEncoderBitrate;
-	bool adaptEncoderComplexity;
-	bool adaptDecoderBitrate;
-	bool adaptDecoderComplexity;
-	
-	int headerType;
-	int packetType;
-} AudioSessionOptions;
 
 class CController
 {
