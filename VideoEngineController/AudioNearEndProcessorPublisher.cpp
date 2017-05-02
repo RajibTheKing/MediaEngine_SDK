c3eb93e0d4fb8065a76e8986668b1bd80599a868
Salah Uddin <sm.salahuddin@ipvisionsoft.com>
2017-05-02 15:10:56 +0600

    Add Audio near end processor publisher.

---
+++ VideoEngineController/AudioNearEndProcessorPublisher.cpp
@@ -0,0 +1,73 @@
+#include "AudioNearEndProcessorPublisher.h"
+#include "LogPrinter.h"
+#include "AudioCallSession.h"
+#include "AudioEncoderBuffer.h"
+
+
+
+AudioNearEndProcessorPublisher::AudioNearEndProcessorPublisher(int nServiceType, int nEntityType, CAudioCallSession *pAudioCallSession, CAudioShortBuffer *pAudioEncodingBuffer, bool bIsLiveStreamingRunning) : 
+AudioNearEndDataProcessor(nServiceType, nEntityType, pAudioCallSession, pAudioEncodingBuffer, bIsLiveStreamingRunning)
+{
+	m_pAudioEncodingBuffer = pAudioEncodingBuffer;
+}
+
+
+
+
+void AudioNearEndProcessorPublisher::ProcessNearEndData()
+{
+	int version = 0;
+	long long llCapturedTime, llRelativeTime = 0, llLasstTime = -1;
+	if (m_pAudioEncodingBuffer->GetQueueSize() == 0)
+		Tools::SOSleep(10);
+	else
+	{
+		LOG18("#18#NE#Publisher...");
+		m_pAudioEncodingBuffer->DeQueue(m_saAudioRecorderFrame, llCapturedTime);
+		LOGT("##NF## encoder got job. time:%lld", llCapturedTime);
+		DumpEncodingFrame();
+
+		int nSendingDataSizeInByte = 1600;	//Or contain 18 bit data with mixed header.
+		bool bIsMuxed = false;
+
+		if (false == PreProcessAudioBeforeEncoding())
+		{
+			return;
+		}
+
+		bIsMuxed = MuxIfNeeded(m_saAudioRecorderFrame, m_saSendingDataPublisher, nSendingDataSizeInByte, m_iPacketNumber);
+
+		UpdateRelativeTimeAndFrame(llLasstTime, llRelativeTime, llCapturedTime);
+
+		memcpy(&m_ucaRawFrameNonMuxed[1 + m_MyAudioHeadersize], m_saSendingDataPublisher, nSendingDataSizeInByte);
+
+		int nSendingFramePacketType = bIsMuxed ? AUDIO_LIVE_PUBLISHER_PACKET_TYPE_MUXED : AUDIO_LIVE_PUBLISHER_PACKET_TYPE_NONMUXED;
+
+		int iSlotID = 0;
+		int iPrevRecvdSlotID = 0;
+		int iReceivedPacketsInPrevSlot = 0;
+		int nChannel = 0;
+		m_ucaRawFrameNonMuxed[0] = 0;
+		BuildAndGetHeaderInArray(nSendingFramePacketType, m_MyAudioHeadersize, 0, iSlotID, m_iPacketNumber, nSendingDataSizeInByte,
+			iPrevRecvdSlotID, iReceivedPacketsInPrevSlot, nChannel, version, llRelativeTime, &m_ucaRawFrameNonMuxed[1]);
+
+		++m_iPacketNumber;
+		if (m_iPacketNumber == m_llMaxAudioPacketNumber)
+		{
+			m_iPacketNumber = 0;
+		}
+		LOG18("#18#NE#Publish  StoreDataForChunk nSendingDataSizeInByte = %d m_MyAudioHeadersize = %d", nSendingDataSizeInByte, m_MyAudioHeadersize);
+
+		int nSendigFrameSize = nSendingDataSizeInByte + m_MyAudioHeadersize + 1;
+		//for (int i = 0; i < 5; i++)
+		//{
+		//	m_ucaRawFrameNonMuxed[m_MyAudioHeadersize + 1 + i] = 110 + i;
+		//	m_ucaRawFrameNonMuxed[nSendigFrameSize - 1 - i] = 110 + i;
+		//}		
+
+		StoreDataForChunk(m_ucaRawFrameNonMuxed, llRelativeTime, nSendigFrameSize);
+
+		Tools::SOSleep(0);
+	}
+}
+
