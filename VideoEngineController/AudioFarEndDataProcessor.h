ba3d624399dc82d615001397c625a5f9e9d45a48
Salah Uddin <sm.salahuddin@ipvisionsoft.com>
2017-05-03 11:15:55 +0600

    Make AudioFarEndDataProcessor as base class.

--- VideoEngineController/AudioFarEndDataProcessor.h
+++ VideoEngineController/AudioFarEndDataProcessor.h
@@ -27,6 +27,9 @@ class AudioFarEndDataProcessor
 public:
 	AudioFarEndDataProcessor(long long llFriendID, int nServiceType, int nEntityType, CAudioCallSession *pAudioCallSession, CCommonElementsBucket* pCommonElementsBucket, bool bIsLiveStreamingRunning);
 	~AudioFarEndDataProcessor();
+
+	virtual	void ProcessFarEndData() = 0;
+
 	int DecodeAudioData(int nOffset, unsigned char *pucaDecodingAudioData, unsigned int unLength, int numberOfFrames, int *frameSizes, std::vector< std::pair<int, int> > &vMissingFrames);
 	void StartCallInLive(int nEntityType);
 	void StopCallInLive(int nEntityType);
@@ -47,43 +50,31 @@ public:
 	CAudioByteBuffer m_AudioReceivedBuffer;
 
 	short tmpBuffer[2048];
+
+
 private:
+
 	int m_nServiceType;
-	int m_nEntityType;
-	long long m_llLastTime;
 	long long m_llFriendID = -1;
 	bool m_bIsLiveStreamingRunning = false;
 	int m_iLastDecodedPacketNumber = -1;
-	int m_nDecodedFrameSize = 0;
-	int m_nDecodingFrameSize = 0;
 	int m_iAudioVersionFriend = -1;
 	//int m_iPrevRecvdSlotID;
 	int m_iCurrentRecvdSlotID = -1;
 	int m_iReceivedPacketsInCurrentSlot = AUDIO_SLOT_SIZE;
-	int m_iOpponentReceivedPackets = AUDIO_SLOT_SIZE;
-
-	std::vector<std::pair<int, int>> m_vFrameMissingBlocks;
 
 	OnFireDataEventCB m_cbOnDataEvent;
 	OnFireNetworkChangeCB m_cbOnNetworkChange;
 	OnFireAudioAlarmCB m_cbOnAudioAlarm;
 
-	AudioMixer* m_pAudioMixer;
-	CAudioCallSession *m_pAudioCallSession = nullptr;
 	CCommonElementsBucket *m_pCommonElementsBucket = nullptr;
 	//AudioPacketHeader *m_pAudioPacketHeader = nullptr;
 	SmartPointer<AudioPacketHeader> m_ReceivingHeader = nullptr;
 	SmartPointer<GomGomGain> m_pGomGomGain;
 
-	std::vector<LiveAudioDecodingQueue*> m_vAudioFarEndBufferVector;
-
-
 	bool m_bAudioDecodingThreadRunning;
 	bool m_bAudioDecodingThreadClosed;
 
-	unsigned char m_ucaDecodingFrame[MAX_AUDIO_FRAME_Length];
-	short m_saDecodedFrame[MAX_AUDIO_FRAME_Length];
-	short m_saCalleeSentData[MAX_AUDIO_FRAME_Length];
 
 	SmartPointer<AudioEncoderInterface> m_pAudioEncoder;
 	SmartPointer<AudioDecoderInterface> m_cAacDecoder;
@@ -95,6 +86,29 @@ private:
 	bool m_bAudioQualityHighNotified;
 	bool m_bAudioShouldStopNotified;
 
+
+protected:
+
+	int m_nEntityType;
+	int m_nDecodingFrameSize = 0;
+	int m_nDecodedFrameSize = 0;
+	int m_iOpponentReceivedPackets = AUDIO_SLOT_SIZE;
+
+	long long m_llLastTime;
+
+	unsigned char m_ucaDecodingFrame[MAX_AUDIO_FRAME_Length];
+	short m_saDecodedFrame[MAX_AUDIO_FRAME_Length];
+	short m_saCalleeSentData[MAX_AUDIO_FRAME_Length];
+
+	std::vector<std::pair<int, int>> m_vFrameMissingBlocks;
+	std::vector<LiveAudioDecodingQueue*> m_vAudioFarEndBufferVector;
+
+	AudioMixer* m_pAudioMixer;
+	CAudioCallSession *m_pAudioCallSession = nullptr;
+
+
+private:
+
 	void StartDecodingThread();
 	static void* CreateAudioDecodingThread(void* param);
 	void StopDecodingThread();
@@ -104,21 +118,28 @@ private:
 	void FarEndProcedureLiveStreamViewer();
 	void FarEndProcedureLiveStreamPublisher();
 	void FarEndProcedureChannel();
-	bool IsQueueEmpty();
-	void DequeueData(int &m_nDecodingFrameSize);
-	void DecodeAndPostProcessIfNeeded(int &iPacketNumber, int &nCurrentPacketHeaderLength, int &nCurrentAudioPacketType);
-	void ParseHeaderAndGetValues(int &packetType, int &nHeaderLength, int &networkType, int &slotNumber, int &packetNumber, int &packetLength, int &recvSlotNumber,
-		int &numPacketRecv, int &channel, int &version, long long &timestamp, unsigned char* header, int &iBlockNumber, int &nNumberOfBlocks, int &iOffsetOfBlock, int &nFrameLength);
-	bool IsPacketTypeSupported(int &nCurrentAudioPacketType);
-	bool IsPacketProcessableBasedOnRole(int &nCurrentAudioPacketType);
-	bool IsPacketProcessableInNormalCall(int &nCurrentAudioPacketType, int &nVersion);
-	bool IsPacketProcessableBasedOnRelativeTime(long long &llCurrentFrameRelativeTime, int &iPacketNumber, int &nPacketType);
-	void SetSlotStatesAndDecideToChangeBitRate(int &nSlotNumber);
-	void PrintDecodingTimeStats(long long &llNow, long long &llTimeStamp, int &iDataSentInCurrentSec,
-		 long long &nDecodingTime, double &dbTotalTime, long long &llCapturedTime);
 
 	void DecideToChangeBitrate(int iNumPacketRecvd);
 	
+
+protected:
+
+	bool IsQueueEmpty();
+	void DequeueData(int &m_nDecodingFrameSize);
+	void ParseHeaderAndGetValues(int &packetType, int &nHeaderLength, int &networkType, int &slotNumber, int &packetNumber, int &packetLength, int &recvSlotNumber,
+		int &numPacketRecv, int &channel, int &version, long long &timestamp, unsigned char* header, int &iBlockNumber, int &nNumberOfBlocks, int &iOffsetOfBlock, int &nFrameLength);
+
+	bool IsPacketTypeSupported(int &nCurrentAudioPacketType);
+	bool IsPacketProcessableInNormalCall(int &nCurrentAudioPacketType, int &nVersion);
+	bool IsPacketProcessableBasedOnRole(int &nCurrentAudioPacketType);
+	bool IsPacketProcessableBasedOnRelativeTime(long long &llCurrentFrameRelativeTime, int &iPacketNumber, int &nPacketType);
+
+	void SetSlotStatesAndDecideToChangeBitRate(int &nSlotNumber);
+	void DecodeAndPostProcessIfNeeded(int &iPacketNumber, int &nCurrentPacketHeaderLength, int &nCurrentAudioPacketType);
+
+	void PrintDecodingTimeStats(long long &llNow, long long &llTimeStamp, int &iDataSentInCurrentSec,
+		 long long &nDecodingTime, double &dbTotalTime, long long &llCapturedTime);
+
 };
 
 #endif //AUDIO_FAREND_DATA_PROCESSOR_H
