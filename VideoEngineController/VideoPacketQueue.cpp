d10548c10f5d05394e62cd6d44db1d96844e9905
Samaun <anayetul@ipvisionsoft.com>
2017-05-14 16:17:31 +0600

    Check onned in VideoPacketQueue's queue procedure

--- VideoEngineController/VideoPacketQueue.cpp
+++ VideoEngineController/VideoPacketQueue.cpp
@@ -40,8 +40,8 @@ int CVideoPacketQueue::Queue(unsigned char *ucaVideoPacketData, int nLength)
 	}
 	else
 	{
-	//	if (nLength >= MAX_VIDEO_PACKET_SIZE)
-	//		return -1;
+		if (nLength >= MAX_VIDEO_PACKET_SIZE)
+			return -1;
 
 		memcpy(m_uc2aVideoPacketBuffer[m_iPushIndex], ucaVideoPacketData, nLength);
 
