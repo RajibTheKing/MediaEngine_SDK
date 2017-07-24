62bc1b956b4d4434d3f490257a3596f0abffd46a
Samaun <anayetul@ipvisionsoft.com>
2017-07-24 17:09:31 +0600

    Release version 9.37.1

--- VideoEngineController/InterfaceOfAudioVideoEngine.cpp
+++ VideoEngineController/InterfaceOfAudioVideoEngine.cpp
@@ -4,8 +4,8 @@
 #include "LogPrinter.h"
 #include "Tools.h"
 
-#define MEDIA_ENGINE_VERSION "9.37"
-#define MEDIA_ENGINE_BUILD_NUMBER 0937002522
+#define MEDIA_ENGINE_VERSION "9.37.1"
+#define MEDIA_ENGINE_BUILD_NUMBER 0937012524
 
 namespace MediaSDK
 {
