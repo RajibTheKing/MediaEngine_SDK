7572ae6ab4d2dae5e1fc8984360a4035096aa98a
Samaun <anayetul@ipvisionsoft.com>
2017-07-31 19:02:34 +0600

    Release version 9.38.1

--- VideoEngineController/CommonMacros.h
+++ VideoEngineController/CommonMacros.h
@@ -4,8 +4,8 @@
 namespace MediaSDK
 {
 
-#define MEDIA_ENGINE_VERSION "9.38"
-#define MEDIA_ENGINE_BUILD_NUMBER 938002531
+#define MEDIA_ENGINE_VERSION "9.38.1"
+#define MEDIA_ENGINE_BUILD_NUMBER 938012531
 
 //Enable to create log
 //#define LOG_ENABLED
