58c121573615174c68a61681742497cc6ff79fff
mansur <mansur@ipvisionsoft.com>
2017-08-12 16:07:35 +0600

    redefined alog macro removed.

--- VideoEngineController/AudioCallSession.h
+++ VideoEngineController/AudioCallSession.h
@@ -14,8 +14,7 @@ namespace MediaSDK
 {
     #define AUDIO_CALL_VERSION  0
     #define AUDIO_LIVE_VERSION  0
-	static std::string colon = "ALOG:";
-	#define ALOG(a) CLogPrinter_WriteSpecific6(CLogPrinter::INFO,colon + a);
+	
 	
 	
 	//#define LOCAL_SERVER_LIVE_CALL
