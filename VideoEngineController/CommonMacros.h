b6875f87fc41da4e71c2d9807058ff12cab1680a
Samaun <anayetul@ipvisionsoft.com>
2017-08-28 19:07:09 +0600

    Release Version = 10.1.3

--- VideoEngineController/CommonMacros.h
+++ VideoEngineController/CommonMacros.h
@@ -4,8 +4,8 @@
 namespace MediaSDK
 {
 
-#define MEDIA_ENGINE_VERSION "10.1.2"
-#define MEDIA_ENGINE_BUILD_NUMBER 1001022623
+#define MEDIA_ENGINE_VERSION "10.1.3"
+#define MEDIA_ENGINE_BUILD_NUMBER 1001032628
 
 	/********Enable to create log************/
 
