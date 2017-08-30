9af4ebf6c2ebf13249edd13e4dff8dde3f0bad84
Maksud Hossain <maksud@ipvisionsoft.com>
2017-08-30 11:47:04 +0600

    [Issue#4241] Opus log modified in ACS.

--- VideoEngineController/AudioCallSession.cpp
+++ VideoEngineController/AudioCallSession.cpp
@@ -69,7 +69,8 @@ namespace MediaSDK
 		m_bNeedToResetEcho(false),
 		m_bIsOpusCodec(bOpusCodec)
 	{
-		MediaLog(LOG_INFO, "[ACS] OpusCodec %d", (int)m_bIsOpusCodec);
+		MediaLog(LOG_DEBUG, "\n[ACS]   ---------------OPUS_ENABLED[%d]----------\n", (int)m_bIsOpusCodec);
+
 		m_recordBuffer = new AudioLinearBuffer(LINEAR_BUFFER_MAX_SIZE);
 
 		m_pAudioCallSessionMutex.reset(new CLockHandler);
