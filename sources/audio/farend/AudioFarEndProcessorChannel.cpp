a80dbe0f4e5427b9af9f19a699af2d3c6b23ea2e
mukcse <mukcse@gmail.com>
2017-10-25 15:25:07 +0600

    [Issue #3627] changed priority from LOG_INFO to LOG_CODE_TRACE for AFEPC

--- sources/audio/farend/AudioFarEndProcessorChannel.cpp
+++ sources/audio/farend/AudioFarEndProcessorChannel.cpp
@@ -28,14 +28,14 @@ namespace MediaSDK
 		long long llTimeStamp = 0;
 		int nQueueSize = m_vAudioFarEndBufferVector[0]->GetQueueSize();
 		m_vFrameMissingBlocks.clear();
-		MediaLog(LOG_INFO, "[FE][AFEPC] QueueSize=%d", nQueueSize);
+		MediaLog(LOG_CODE_TRACE, "[FE][AFEPC] QueueSize=%d", nQueueSize);
 		if (nQueueSize > 0)
 		{
 			m_nDecodingFrameSize = m_vAudioFarEndBufferVector[0]->DeQueue(m_ucaDecodingFrame, m_vFrameMissingBlocks);
-			MediaLog(LOG_INFO, "[FE][AFEPC] BeforeDecoding -> FrameSize=%d, #(FrameMissingBlocks)=%d", m_nDecodingFrameSize, (int)m_vFrameMissingBlocks.size());
+			MediaLog(LOG_CODE_TRACE, "[FE][AFEPC] BeforeDecoding -> FrameSize=%d, #(FrameMissingBlocks)=%d", m_nDecodingFrameSize, (int)m_vFrameMissingBlocks.size());
 			if (m_nDecodingFrameSize < 1)
 			{
-				MediaLog(LOG_INFO, "[FE][AFEPC] BeforeDecoding -> Removed for FrameSize<1");
+				MediaLog(LOG_CODE_TRACE, "[FE][AFEPC] BeforeDecoding -> Removed for FrameSize<1");
 				return;
 			}
 
@@ -49,25 +49,25 @@ namespace MediaSDK
 			ParseHeaderAndGetValues(nCurrentAudioPacketType, nCurrentPacketHeaderLength, dummy, iPacketNumber, nPacketDataLength,
 				nChannel, nVersion, llRelativeTime, m_ucaDecodingFrame, iBlockNumber, nNumberOfBlocks, iOffsetOfBlock, nFrameLength, nEchoStateFlags);
 
-			MediaLog(LOG_INFO, "[FE][AFEPC] BeforeDecoding:ParsedData -> RelativeTime=%lld, PN=%d, PDL=%d, FrameLength=%d", llRelativeTime, iPacketNumber, nPacketDataLength, nFrameLength);
+			MediaLog(LOG_CODE_TRACE, "[FE][AFEPC] BeforeDecoding:ParsedData -> RelativeTime=%lld, PN=%d, PDL=%d, FrameLength=%d", llRelativeTime, iPacketNumber, nPacketDataLength, nFrameLength);
 			if (!IsPacketProcessableBasedOnRole(nCurrentAudioPacketType))
 			{
-				MediaLog(LOG_INFO, "[FE][AFEPC] BeforeDecoding -> Removed based on packet type & corresponding role");
+				MediaLog(LOG_CODE_TRACE, "[FE][AFEPC] BeforeDecoding -> Removed based on packet type & corresponding role");
 				return;
 			}
 
 			bool bIsCompleteFrame = true;
 			llNow = Tools::CurrentTimestamp();
 			bIsCompleteFrame = m_pAudioDePacketizer->dePacketize(m_ucaDecodingFrame + nCurrentPacketHeaderLength, iBlockNumber, nNumberOfBlocks, nPacketDataLength, iOffsetOfBlock, iPacketNumber, nFrameLength, llNow, m_llLastTime);
-			MediaLog(LOG_INFO, "[FE][AFEPC] BeforeDecoding:AfterDePacketize -> PN=%d, PDL=%d, BN=%d, FrameLength=%d", iPacketNumber, nPacketDataLength, iBlockNumber, nFrameLength);
+			MediaLog(LOG_CODE_TRACE, "[FE][AFEPC] BeforeDecoding:AfterDePacketize -> PN=%d, PDL=%d, BN=%d, FrameLength=%d", iPacketNumber, nPacketDataLength, iBlockNumber, nFrameLength);
 
 			if (bIsCompleteFrame){
-				MediaLog(LOG_INFO, "[FE][AFEPC] Complete[P=%d B=%d]", iPacketNumber, iBlockNumber);
+				MediaLog(LOG_CODE_TRACE, "[FE][AFEPC] Complete[P=%d B=%d]", iPacketNumber, iBlockNumber);
 
 				m_nDecodingFrameSize = m_pAudioDePacketizer->GetCompleteFrame(m_ucaDecodingFrame + nCurrentPacketHeaderLength) + nCurrentPacketHeaderLength;
 				if (!IsPacketProcessableBasedOnRelativeTime(llRelativeTime, iPacketNumber, nCurrentAudioPacketType))
 				{
-					MediaLog(LOG_INFO, "[FE][AFEPC] BeforeDecoding ->  Removed based on relative time");
+					MediaLog(LOG_CODE_TRACE, "[FE][AFEPC] BeforeDecoding ->  Removed based on relative time");
 					return;
 				}
 			}
@@ -75,19 +75,19 @@ namespace MediaSDK
 
 
 			if (bIsCompleteFrame){
-				MediaLog(LOG_INFO, "[FE][AFEPC] WORKING ON COMPLETE FRAME . ");
+				MediaLog(LOG_CODE_TRACE, "[FE][AFEPC] WORKING ON COMPLETE FRAME . ");
 				m_nDecodingFrameSize -= nCurrentPacketHeaderLength;
-				MediaLog(LOG_INFO, "[FE][AFEPC] BeforeDecoding -> DecodingFrameSize=%d, PN=%d, PT=%d, PHL=%d", m_nDecodingFrameSize, iPacketNumber, nCurrentAudioPacketType, nCurrentPacketHeaderLength);
+				MediaLog(LOG_CODE_TRACE, "[FE][AFEPC] BeforeDecoding -> DecodingFrameSize=%d, PN=%d, PT=%d, PHL=%d", m_nDecodingFrameSize, iPacketNumber, nCurrentAudioPacketType, nCurrentPacketHeaderLength);
 				DecodeAndPostProcessIfNeeded(iPacketNumber, nCurrentPacketHeaderLength, nCurrentAudioPacketType);
 				DumpDecodedFrame(m_saDecodedFrame, m_nDecodedFrameSize);
 				PrintDecodingTimeStats(llNow, llTimeStamp, iDataSentInCurrentSec, nDecodingTime, dbTotalTime, llCapturedTime);
-				MediaLog(LOG_INFO, "[FE][AFEPC] AfterDecoding -> DecodedFrameSize=%d", m_nDecodedFrameSize);
+				MediaLog(LOG_CODE_TRACE, "[FE][AFEPC] AfterDecoding -> DecodedFrameSize=%d", m_nDecodedFrameSize);
 				if (m_nDecodedFrameSize < 1)
 				{
-					MediaLog(LOG_INFO, "[FE][AFEPC] Removed for DecodedFrameSize<1");
+					MediaLog(LOG_CODE_TRACE, "[FE][AFEPC] Removed for DecodedFrameSize<1");
 					return;
 				}
-				MediaLog(LOG_INFO, "[FE][AFEPC] ChannelSendToPlayer");
+				MediaLog(LOG_CODE_TRACE, "[FE][AFEPC] ChannelSendToPlayer");
 				SendToPlayer(m_saDecodedFrame, m_nDecodedFrameSize, m_llLastTime, iPacketNumber, nEchoStateFlags);
 				Tools::SOSleep(0);
 			}
