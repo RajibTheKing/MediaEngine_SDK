694b84ba438668450b118900dfc56ddff925c00a
Maksud Hossain <maksud@ipvisionsoft.com>
2017-11-05 11:37:05 +0600

    Release Version = 12.10

--- sources/common/CommonMacros.h
+++ sources/common/CommonMacros.h
@@ -6,8 +6,8 @@
 namespace MediaSDK
 {
 
-#define MEDIA_ENGINE_VERSION "12.9"
-#define MEDIA_ENGINE_BUILD_NUMBER 1209002823
+#define MEDIA_ENGINE_VERSION "12.10"
+#define MEDIA_ENGINE_BUILD_NUMBER 1210002905
 
 	/********Enable to create log************/
 
