9a348b1c02111fa312664bfba52d85158b2b6abf
mukcse <mukcse@gmail.com>
2017-11-13 17:31:21 +0600

    [Issue #2983] refactoring in AudioDumper to maintain SRP(Single Responsibility Principle)

--- sources/utility/AudioDumper.cpp
+++ sources/utility/AudioDumper.cpp
@@ -5,17 +5,11 @@ namespace MediaSDK
 #define DATE_TIME_LENGTH 10
 	CAudioDumper::CAudioDumper(std::string fileName, bool enable)
 	{
-		std::string filePath;
-#if defined(__ANDROID__)
-		filePath = "/sdcard/";
-#elif defined(TARGET_OS_IPHONE) || defined(TARGET_IPHONE_SIMULATOR)
-		filePath = std::string(getenv("HOME")) + "/Documents/";
-#elif defined(DESKTOP_C_SHARP)
-		filePath = "";
-#endif
+		std::string Directory = Tools::GetCurrentDirectoryAny();
 		char prefix[DATE_TIME_LENGTH];
-		Tools::GetDateTime(prefix);
-		std::string filePathwithFileName = filePath + std::string(prefix) + fileName;
+		Tools::GetTime(prefix);
+		std::string filePathwithFileName = Directory + std::string(prefix) + "_" + fileName;
+
 		if (enable) dumpfile = fopen(filePathwithFileName.c_str(), "wb");
 	}
 
