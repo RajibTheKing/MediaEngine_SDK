015d33e7d535ce50885267a0227cc138193c609a
nayeem <vampireegg@gmail.com>
2017-11-20 13:02:15 +0600

    Despike added.

--- sources/audio/dsp/KichCutter.cpp
+++ sources/audio/dsp/KichCutter.cpp
@@ -1,6 +1,8 @@
 
 #include "KichCutter.h"
 #include "AudioMacros.h"
+#include "string.h"
+#include "stdlib.h"
 
 KichCutter::KichCutter()
 {
@@ -10,12 +12,14 @@ KichCutter::KichCutter()
 	m_bRemoveContinentalShelves = false;
 	m_nDataBufSize = MAX_AUDIO_FRAME_SAMPLE_SIZE * 3; //2400
 	m_nDataBufIOSize = MAX_AUDIO_FRAME_SAMPLE_SIZE; //800
-	m_sTotalSpikeSampleCount = m_sSpikeSampleCount + 2 * m_sZeroSampleCount;
+	m_sTotalSpikeSampleCount = m_sSpikeSampleCount + 2 * m_sZeroSampleCount; //1100
+	m_nDataBufCopySize = m_nDataBufSize - m_nDataBufIOSize; //1600
 
 	m_sSilentBuf = new short[m_sZeroSampleCount]();
 	m_sSilentFillBuf = new short[m_sTotalSpikeSampleCount]();
 	m_sTempBuf = new short[m_sTotalSpikeSampleCount]();
 	m_sDataBuf = new short[m_nDataBufSize]();
+
 }
 
 KichCutter::~KichCutter()
@@ -24,4 +28,54 @@ KichCutter::~KichCutter()
 	if (m_sSilentFillBuf) delete[] m_sSilentFillBuf;
 	if (m_sTempBuf) delete[] m_sTempBuf;
 	if (m_sDataBuf) delete[] m_sDataBuf;
+}
+
+void KichCutter::Despike(short *sBuffer)
+{
+	memcpy(m_sDataBuf, m_sDataBuf + m_nDataBufIOSize, m_nDataBufCopySize * sizeof(short));
+	memcpy(m_sDataBuf + m_nDataBufCopySize, sBuffer, m_nDataBufIOSize * sizeof(short));
+
+	for (int i = 0; i < m_nDataBufSize - m_sTotalSpikeSampleCount; i++)
+	{
+		memcpy(m_sTempBuf, m_sDataBuf + i, sizeof(short) * m_sTotalSpikeSampleCount);
+		for (int j = 0; j < m_sSpikeSampleCount; j++)
+		{
+			if (abs(m_sTempBuf[j]) < m_sThreshold)
+			{
+				m_sTempBuf[j] = 0;
+			}
+		}
+		if (m_bRemoveContinentalShelves)
+		{
+			memset(m_sTempBuf + m_sTotalSpikeSampleCount - m_sZeroSampleCount, 0, m_sZeroSampleCount * sizeof(short));
+		}
+
+		bool bLeadingZeroFound = false, bTrailingZeroFound = false;
+
+		if (memcmp(m_sTempBuf, m_sSilentBuf, m_sZeroSampleCount * sizeof(short)) == 0)
+		{
+			bLeadingZeroFound = true;
+		}
+
+		if (!bLeadingZeroFound) continue;
+
+		int j = 1;
+		for (; j <= m_sSpikeSampleCount; j++)
+		{
+			if (memcmp(m_sTempBuf + m_sZeroSampleCount + j, m_sSilentBuf, m_sZeroSampleCount * sizeof(short)) == 0)
+			{
+				bTrailingZeroFound = true;
+				break;
+			}
+		}
+
+		if (bLeadingZeroFound && bTrailingZeroFound)
+		{
+			memcpy(m_sDataBuf + i, m_sSilentFillBuf, sizeof(short) * (2 * m_sZeroSampleCount + j));
+			i = i + m_sZeroSampleCount + j;
+		}
+
+	}
+
+	memcpy(sBuffer, m_sDataBuf, m_nDataBufIOSize * sizeof(short));
 }
--- sources/audio/dsp/KichCutter.h
+++ sources/audio/dsp/KichCutter.h
@@ -5,6 +5,7 @@ class KichCutter
 public:
 	KichCutter();
 	~KichCutter();
+	void Despike(short *sBuffer);
 
 private:
 	short m_sZeroSampleCount;
@@ -14,6 +15,8 @@ private:
 	bool m_bRemoveContinentalShelves;
 	int m_nDataBufSize;
 	int m_nDataBufIOSize;
+	int m_nDataBufCopySize;
+
 
 	short *m_sSilentBuf;
 	short *m_sSilentFillBuf;
