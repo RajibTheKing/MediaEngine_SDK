9e4ef86d45927f2e4fb00850c7d8eb2d5ec8d6b4
Samaun <anayetul@ipvisionsoft.com>
2018-05-14 19:32:17 +0600

    fixed desktop crash

--- sources/video/Converter/ColorConverter.h
+++ sources/video/Converter/ColorConverter.h
@@ -116,7 +116,7 @@ namespace MediaSDK
         int RotateI420(unsigned char *pInput, int inHeight, int inWidth, unsigned char *pOutput, int &outHeight, int &outWidth, int rotationParameter);
 	
 	
-		unsigned char m_pSSSmallFrame[(MAX_FRAME_HEIGHT * MAX_FRAME_WIDTH) >> 1];
+		unsigned char m_pSSSmallFrame[MAX_FRAME_HEIGHT * MAX_FRAME_WIDTH];
 		int m_nOponentHeight;
 		int m_nOponentWidth;
 
