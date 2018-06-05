23dac1983cc4e3d0fde7522cc910994a734f78dc
Samaun <anayetul@ipvisionsoft.com>
2018-06-05 12:49:15 +0600

    made adaptive to the future changes 2

--- sources/video/Thread/VideoDecodingThread.cpp
+++ sources/video/Thread/VideoDecodingThread.cpp
@@ -863,11 +863,6 @@ namespace MediaSDK
 								this->m_pColorConverter->Crop_YUV420(m_ucaTempTempFrame, iHeight / 2, iWidth / 2, 0, 0, nHeightDiff / 2, nHeightDiff / 2, m_ucaTempFrame2, nSmallFrameModifiedHeight, nSmallFrameModifiedWidth);
 
 								this->m_pColorConverter->Merge_Two_Video2(false, m_DecodedFrame, iWidth / 2, upperOffset, iHeight, iWidth, m_ucaTempFrame2, iHeight / 2 - nHeightDiff, iWidth / 2);
-
-
-								this->m_pColorConverter->DownScaleYUV420_Dynamic_Version222(m_ucaOpponentSmallFrame, nSmallFrameHeight, nSmallFrameWidth, m_ucaTempFrame2, iHeight / 2, iWidth / 2);
-
-								this->m_pColorConverter->Merge_Two_Video2(false, m_DecodedFrame, iWidth / 2, upperOffset, iHeight, iWidth, m_ucaTempFrame2, iHeight / 2, iWidth / 2);
 							}
 							else
 							{
